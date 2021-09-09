3869f8579e06fabfffb716c5f331ee87210fa2b8
7d <61975820+iiiii7d@users.noreply.github.com>
2021-09-09 15:16:07 +0800

    component info screen, name, displayname and description

---
+++ scripts/sidebar.js
@@ -0,0 +1,30 @@
+var sidebar = L.control.sidebar({
+  autopan: false, 
+  closeButton: true, 
+  container: 'sidebar',
+  position: 'left',
+}).addTo(map);
+
+sidebar.addPanel({
+  id: 'pane_welcome',
+  tab: '<i class="fas fa-door-open"></i>',
+  pane: document.getElementById("welcome").innerHTML,
+  title: 'Welcome to Stencil ' + VERSION
+});
+
+sidebar.addPanel({
+  id: 'pane_componentInfo',
+  tab: '<i class="fas fa-draw-polygon"></i>',
+  pane: document.getElementById("componentInfo").innerHTML,
+  title: 'Component Info'
+});
+
+/*sidebar.addPanel({
+  id: 'discordLogin',
+  tab: '<i class="fab fa-discord"></i>',
+  pane: document.getElementById("discordLogin").innerHTML,
+  title: 'Discord Login',
+  position: 'bottom'
+});*/
+
+sidebar.open('pane_welcome');
