11a3adf2d82211a0c916d19662681532522a4264
7d <61975820+iiiii7d@users.noreply.github.com>
2021-09-10 00:27:21 +0800

    saving of data edited in component info panel, disabling of context menu, disabling of component info panel when not selected

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,3 +1,5 @@
+var selected = null
+
 var layers = L.layerGroup([]);
 map.addLayer(layers);
 map.pm.setGlobalOptions({
@@ -25,36 +27,84 @@ map.on("pm:create", e => {
       nodes: [],
       attrs: {}
     }
+    console.log("Created")
 
     e.layer.on("click", e => {
-        document.getElementById("pane_componentInfo").querySelector("div").innerHTML = "";
-        document.getElementById("c_id").innerHTML = e.target.mapInfo.id;
-        document.getElementById("c_displayname").innerHTML = e.target.mapInfo.displayname;
-        document.getElementById("c_description").innerHTML = e.target.mapInfo.description;
-
-        document.getElementById("pane_componentInfo").querySelector("div").innerHTML = document.getElementById("componentInfo").querySelector("div").innerHTML;
-
-        document.getElementById("c_add-attr").onclick = () => {
-          let element = document.createElement('tr');
-          element.innerHTML = document.getElementById("c_attr-row").innerHTML;
-          element.querySelector(".c_attr-delete").onclick = e => {
-            e.target.parentElement.remove()
-            e.target.parentElement.innerHTML = "";
+      // generates a row of attrs
+      function genTr(timestamp, name, value) {
+        let element = document.createElement('tr');
+        element.innerHTML = document.getElementById("c_attr-row").innerHTML;
+        element.setAttribute("name", timestamp ? timestamp : new Date().toUTCString());
+        element.querySelector(".c_attr-name").innerHTML = name ? name : "";
+        element.querySelector(".c_attr-value").innerHTML = value ? value : "";
+        element.querySelector(".c_attr-delete").onclick = e => {
+          e.target.parentElement.remove()
+          e.target.parentElement.innerHTML = "";
+        }
+        element.querySelector(".c_attr-delete").querySelector("i").onclick = e => {
+          delete selector.mapInfo.attrs[e.target.parentElement.parentElement.getAttr("name")]
+          e.target.parentElement.parentElement.remove()
+          e.target.parentElement.parentElement.innerHTML = "";
+        }
+        element.querySelectorAll(".c_attr-name, .c_attr-value").forEach(element => { //adds saving for attrs
+          element.onblur = () => {
+            rowElements = document.getElementById("c_attr").querySelectorAll("tr");
+            nameElements = document.getElementById("c_attr").querySelectorAll(".c_attr-name");
+            valueElements = document.getElementById("c_attr").querySelectorAll(".c_attr-value");
+    
+            for (i=0; i < rowElements.length; i++) {
+              selected.mapInfo.attrs[rowElements[i].getAttribute("name")] = {
+                name: nameElements[i].value,
+                value: valueElements[i].value
+              }
+            }
           }
-          element.querySelector(".c_attr-delete").querySelector("i").onclick = e => {
-            e.target.parentElement.parentElement.remove()
-            e.target.parentElement.parentElement.innerHTML = "";
-          }
-          document.getElementById("c_attr").appendChild(element)
-        };
+        })
+        return element
+      }
 
-        /*sidebar.removePanel('pane_componentInfo');
-        sidebar.addPanel({
-          id: 'pane_componentInfo',
-          tab: '<i class="fas fa-draw-polygon"></i>',
-          pane: document.getElementById("componentInfo").innerHTML,
-          title: 'Component Info'
-        });*/
-        sidebar.open('pane_componentInfo');
+      selected = e.target;
+      document.getElementById("pane_componentInfo").querySelector("div").innerHTML = ""; // clear the pane
+      document.getElementById("c_id").innerHTML = selected.mapInfo.id; // sets id, displayname, description, layer
+      document.getElementById("c_displayname").innerHTML = selected.mapInfo.displayname;
+      document.getElementById("c_description").innerHTML = selected.mapInfo.description;
+      document.getElementById("c_layer").innerHTML = selected.mapInfo.layer;
+
+      document.getElementById("c_attr").innerHTML = ""; //sets attrs
+      let attrs = selected.mapInfo.attrs
+      Object.keys(attrs).map(key => [key, attrs[key]]).forEach(([timestamp, info]) => {
+        element = genTr(timestamp, info.name, info.value);
+        document.getElementById("c_attr").appendChild(element);
+      })
+
+      // adds content to pane
+      document.getElementById("pane_componentInfo").querySelector("div").innerHTML = document.getElementById("componentInfo").querySelector("div").innerHTML;
+
+      document.getElementById("c_add-attr").onclick = () => { //adds add attribute button functionality
+        element = genTr()
+        document.getElementById("c_attr").appendChild(element)
+      };
+      
+      // TODO c_type
+
+      ["id", "displayname", "description"].forEach(property => { //adds saving and filter for id, displayname, description
+        document.getElementById("c_"+property).onblur = () => {
+          document.getElementById("c_"+property).innerHTML = document.getElementById("c_"+property).innerHTML.replaceAll(/<br>/gm, "");
+          selected.mapInfo[property] = document.getElementById("c_"+property).innerHTML;
+        }
+      })
+
+      document.getElementById("c_layer").onblur = () => { //adds saving and filter for layer
+        document.getElementById("c_layer").innerHTML = parseFloat(document.getElementById("c_layer").innerHTML.replaceAll(/(?:(?<=^.+)-|[^\d-\.])/gm, "")).toString();
+        selected.mapInfo.layer = parseFloat(document.getElementById("c_layer").innerHTML);
+      }
+
+      sidebar.enablePanel('pane_componentInfo');
+      sidebar.open('pane_componentInfo'); // opens the pane
     });
+
+    e.layer.off("click", e => {
+      selected = null;
+      sidebar.disablePanel('pane_componentInfo');
+    })
 });
