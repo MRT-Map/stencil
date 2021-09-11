177c7edb6a4ad51168dbc13ffd13628f8375993f
7d <61975820+iiiii7d@users.noreply.github.com>
2021-09-11 11:10:43 +0000

    completed type picker

--- scripts/getskin.js
+++ scripts/getskin.js
@@ -14,3 +14,46 @@ $.ajax({
       };
     }
 });
+
+function getFrontColor(type) {
+  let typeLayers = Object.values(Skin.types[type].style)[0];
+  let filteredLayers;
+  switch (Skin.types[type].type) {
+    case "point":
+      filteredLayers = typeLayers.filter(typeLayer => ['circle', 'square'].indexOf(typeLayer.layer) != -1);
+      if (filteredLayers.length == 0) return "#808080"
+      else return filteredLayers[filteredLayers.length-1].colour;
+      break;
+    case "line":
+      filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "fore");
+      return filteredLayers[filteredLayers.length-1].colour;
+      break;
+    case "area":
+      filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "fill");
+      return filteredLayers[filteredLayers.length-1].colour;
+      break;
+    default:
+      return undefined;
+  }
+}
+
+function getBackColor(type) {
+  let typeLayers = Object.values(Skin.types[type].style)[0];
+  let filteredLayers;
+  switch (Skin.types[type].type) {
+    case "point":
+      return null;
+      break;
+    case "line":
+      filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "back");
+      if (filteredLayers.length != 0) return filteredLayers[filteredLayers.length-1].colour;
+      else return null;
+      break;
+    case "area":
+      filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "fill");
+      return filteredLayers[filteredLayers.length-1].outline;
+      break;
+    default:
+      return undefined;
+  }
+}
