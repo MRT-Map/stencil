b0ace05ff2d3664e453a8039bc720e57108c2bd0
7d <61975820+iiiii7d@users.noreply.github.com>
2022-01-07 18:04:53 +0800

    js to ts

--- scripts/getskin.js
+++ scripts/getskin.js
@@ -1,75 +1,80 @@
-Skin = null;
-ComponentTypes = null;
+/// <reference path="references.ts" />
+var Skin = null;
+var ComponentTypes = null;
+var $;
 $.ajax({
     async: false,
     url: "https://raw.githubusercontent.com/MRT-Map/tile-renderer/main/renderer/skins/default.json",
-    success: data => {
-      json = JSON.parse(data);
-      Skin = json;
-      ComponentTypes = {
-        all: Object.keys(json.types),
-        point: Object.keys(json.types).filter(type => json.types[type].type == "point"),
-        line: Object.keys(json.types).filter(type => json.types[type].type == "line"),
-        area: Object.keys(json.types).filter(type => json.types[type].type == "area")
-      };
+    success: function (data) {
+        var json = JSON.parse(data);
+        Skin = json;
+        ComponentTypes = {
+            all: Object.keys(json.types),
+            point: Object.keys(json.types).filter(function (type) { return json.types[type].type == "point"; }),
+            line: Object.keys(json.types).filter(function (type) { return json.types[type].type == "line"; }),
+            area: Object.keys(json.types).filter(function (type) { return json.types[type].type == "area"; })
+        };
     }
 });
-
 function getFrontColor(type) {
-  let typeLayers = Object.values(Skin.types[type].style)[0];
-  let filteredLayers;
-  switch (Skin.types[type].type) {
-    case "point":
-      filteredLayers = typeLayers.filter(typeLayer => ['circle', 'square'].indexOf(typeLayer.layer) != -1);
-      if (filteredLayers.length == 0) return "#808080";
-      else return filteredLayers[filteredLayers.length-1].colour;
-      break;
-    case "line":
-      filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "fore");
-      return filteredLayers[filteredLayers.length-1].colour;
-    case "area":
-      filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "fill");
-      return filteredLayers[filteredLayers.length-1].colour;
-    default:
-      return undefined;
-  }
+    var typeLayers = Object.values(Skin.types[type].style)[0]; // TODO type for PLA thingy
+    var filteredLayers;
+    switch (Skin.types[type].type) {
+        case "point":
+            filteredLayers = typeLayers.filter(function (typeLayer) { return ['circle', 'square'].indexOf(typeLayer.layer) != -1; });
+            if (filteredLayers.length == 0)
+                return "#808080";
+            else
+                return filteredLayers[filteredLayers.length - 1].colour;
+            break;
+        case "line":
+            filteredLayers = typeLayers.filter(function (typeLayer) { return typeLayer.layer == "fore"; });
+            return filteredLayers[filteredLayers.length - 1].colour;
+        case "area":
+            filteredLayers = typeLayers.filter(function (typeLayer) { return typeLayer.layer == "fill"; });
+            return filteredLayers[filteredLayers.length - 1].colour;
+        default:
+            return undefined;
+    }
 }
-
 function getBackColor(type) {
-  let typeLayers = Object.values(Skin.types[type].style)[0];
-  let filteredLayers;
-  switch (Skin.types[type].type) {
-    case "point":
-      return null;
-    case "line":
-      filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "back");
-      if (filteredLayers.length != 0) return filteredLayers[filteredLayers.length-1].colour;
-      else return null;
-      break;
-    case "area":
-      filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "fill");
-      return filteredLayers[filteredLayers.length-1].outline;
-    default:
-      return undefined;
-  }
+    var typeLayers = Object.values(Skin.types[type].style)[0];
+    var filteredLayers;
+    switch (Skin.types[type].type) {
+        case "point":
+            return null;
+        case "line":
+            filteredLayers = typeLayers.filter(function (typeLayer) { return typeLayer.layer == "back"; });
+            if (filteredLayers.length != 0)
+                return filteredLayers[filteredLayers.length - 1].colour;
+            else
+                return null;
+            break;
+        case "area":
+            filteredLayers = typeLayers.filter(function (typeLayer) { return typeLayer.layer == "fill"; });
+            return filteredLayers[filteredLayers.length - 1].outline;
+        default:
+            return undefined;
+    }
 }
-
 function getWeight(type) {
-  let typeLayers = Object.values(Skin.types[type].style)[0];
-  let filteredLayers;
-  switch (Skin.types[type].type) {
-    case "point":
-      return null;
-    case "line":
-      filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "fore");
-      if (filteredLayers.length != 0) return filteredLayers[filteredLayers.length-1].width/2**(8-map.getZoom()+1);
-      else return null;
-      break;
-    case "area":
-      /*filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "fill");
-      return filteredLayers[filteredLayers.length-1].outline;*/
-      return 2/2**(8-map.getZoom()+1);
-    default:
-      return undefined;
-  }
+    var typeLayers = Object.values(Skin.types[type].style)[0];
+    var filteredLayers;
+    switch (Skin.types[type].type) {
+        case "point":
+            return null;
+        case "line":
+            filteredLayers = typeLayers.filter(function (typeLayer) { return typeLayer.layer == "fore"; });
+            if (filteredLayers.length != 0)
+                return filteredLayers[filteredLayers.length - 1].width / Math.pow(2, (8 - map.getZoom() + 1));
+            else
+                return null;
+            break;
+        case "area":
+            /*filteredLayers = typeLayers.filter(typeLayer => typeLayer.layer == "fill");
+            return filteredLayers[filteredLayers.length-1].outline;*/
+            return 2 / Math.pow(2, (8 - map.getZoom() + 1));
+        default:
+            return undefined;
+    }
 }
--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,33 +1,30 @@
+/// <reference path="references.ts" />
+var L;
 console.log(Skin);
-
 var sidebar = L.control.sidebar({
-  autopan: false, 
-  closeButton: true, 
-  container: 'sidebar',
-  position: 'left',
+    autopan: false,
+    closeButton: true,
+    container: 'sidebar',
+    position: 'left'
 }).addTo(map);
-
 sidebar.addPanel({
-  id: 'pane_welcome',
-  tab: '<i class="fas fa-door-open"></i>',
-  pane: document.getElementById("welcome").innerHTML,
-  title: 'Welcome to Stencil ' + VERSION
+    id: 'pane_welcome',
+    tab: '<i class="fas fa-door-open"></i>',
+    pane: document.getElementById("welcome").innerHTML,
+    title: 'Welcome to Stencil ' + VERSION
 });
-
 sidebar.addPanel({
-  id: 'pane_componentInfo',
-  tab: '<i class="fas fa-draw-polygon"></i>',
-  pane: '<div><h1>Select a component...</h1></div>',
-  title: 'Component Info'
+    id: 'pane_componentInfo',
+    tab: '<i class="fas fa-draw-polygon"></i>',
+    pane: '<div><h1>Select a component...</h1></div>',
+    title: 'Component Info'
 });
-
 sidebar.addPanel({
-  id: 'pane_typePicker',
-  tab: '<i class="fas fa-palette"></i>',
-  pane: document.getElementById("typePicker").innerHTML,
-  title: 'Pick Type'
+    id: 'pane_typePicker',
+    tab: '<i class="fas fa-palette"></i>',
+    pane: document.getElementById("typePicker").innerHTML,
+    title: 'Pick Type'
 });
-
 /*sidebar.addPanel({
   id: 'discordLogin',
   tab: '<i class="fab fa-discord"></i>',
@@ -35,5 +32,4 @@ sidebar.addPanel({
   title: 'Discord Login',
   position: 'bottom'
 });*/
-
 sidebar.open('pane_welcome');
