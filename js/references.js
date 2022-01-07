551057301f68075dca4ed7881cf17d32c972de19
7d <61975820+iiiii7d@users.noreply.github.com>
2022-01-07 21:33:25 +0800

    ts migration complete

--- js/references.js
+++ js/references.js
@@ -1,3 +1,5 @@
+/// <reference path="../node_modules/leaflet-sidebar-v2/index.d.ts" />
+/// <reference path="../node_modules/@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.d.ts" />
 /// <reference path="getskin.ts" />
 /// <reference path="map.ts" />
 /// <reference path="sidebar.ts" />
