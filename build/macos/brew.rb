d88cc01389850ecd046dd4661d1178eee03776ac
7d <61975820+iiiii7d@users.noreply.github.com>
2026-04-18 20:22:52 +0800

    fix homebrew and arch pkgbuild

--- build/macos/brew.rb
+++ build/macos/brew.rb
@@ -2,7 +2,7 @@ cask "stencil3" do
   version "3.0.0-alpha.0"
   sha256 "0e8d849576f1ffb5a88237375a7b6cd198de81e25aa8a34fe84ec78ee46fe1dc"
 
-  url "https://github.com/MRT-Map/stencil3/releases/download/v#{version}/stencil3.dmg"
+  url "https://github.com/MRT-Map/stencil3/releases/download/v#{version}/stencil3-#{version}.dmg"
   name "stencil3"
   desc "Map editor for MRT Map data"
   homepage "https://github.com/MRT-Map/stencil3"
@@ -11,7 +11,8 @@ cask "stencil3" do
   binary "#{appdir}/stencil3.app/Contents/MacOS/stencil3"
 
   zap trash: [
-    "~/Library/Application Support/stencil3",
-    "~/Library/Caches/stencil3",
+    "~/Library/Application Support/io.github.mrt-map.stencil3",
+    "~/Library/Caches/io.github.mrt-map.stencil3",
+    "~/Library/Preferences/io.github.mrt-map.stencil3",
   ]
 end
